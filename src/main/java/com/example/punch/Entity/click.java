355698e4dfacad8d4a5a9460e2303ba40172cd60
rhtnddd <qlalfql123@gmail.com>
2026-04-20 08:25:12 +0900

    Entity완성

--- src/main/java/com/example/punch/Entity/click.java
+++ src/main/java/com/example/punch/Entity/click.java
@@ -1,18 +1,31 @@
 package com.example.punch.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "click")
 public class click {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int clicks;
-    private boolean ban;
     @Column(unique = true)
     private String username;
     private String password;
+
+    public click(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.clicks = 0;
+    }
+
+    public void plusClick() {
+        this.clicks++;
+    }
 }
