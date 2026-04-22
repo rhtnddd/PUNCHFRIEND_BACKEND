d6c43bf505b85e2503e554aa446e551769f7f898
rhtnddd <qlalfql123@gmail.com>
2026-04-22 17:50:41 +0900

    add: 유저이름 찾기 기능

--- src/main/java/com/example/punch/Repository/Userrepository.java
+++ src/main/java/com/example/punch/Repository/Userrepository.java
@@ -1,6 +1,7 @@
 package com.example.punch.Repository;
 
 import com.example.punch.Entity.click;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface Userrepository extends JpaRepository<click, Long> {
+    Optional<click> findByUsername(String username);
     Optional<click> findByUsernameAndPassword(String username, String password);
 }
